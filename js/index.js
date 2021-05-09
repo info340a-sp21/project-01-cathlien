9e829b9ca49d1d268de6e984f17306106810f0d5
cathlien <cathlien@uw.edu>
2021-05-09 13:22:52 +0800

    when pressed the button to see data, data displays in a table

--- js/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function displayData() {
-    d3.csv("data/grad-students.csv").then(function(data) {
-        console.log(data); // [{"Hello": "world"}, …]
-      });
-}
-
-displayData();
--- js/resource.js
+++ js/resource.js
@@ -2,12 +2,30 @@
 
 
 const formElem = document.querySelector('form');
+const tableBody = document.querySelector('tbody');
 formElem.addEventListener('submit', function(event){
     event.preventDefault();
-    d3.csv("data/grad-students.csv").then(function(data) {
-        console.log(data);
+    loadData();
+});
+
+function loadData() {
+    let promise = d3.csv("data/grad-students.csv").then(function(data) {
+        for (let i = 0; i < data.length; i++) {
+            let row = document.createElement('tr');
+            row.appendChild(createRowNode(data[i].Major));
+            row.appendChild(createRowNode(data[i].Grad_total));
+            row.appendChild(createRowNode(data[i].Grad_employed));
+            tableBody.appendChild(row);
+        }
     })
     .catch(function(error){
         console.error(error);
     });
-});
+    return promise;
+}
+
+function createRowNode(text) {
+    let node = document.createElement('td');
+    node.textContent = text;
+    return node;
+}
